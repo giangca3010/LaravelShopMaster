d46d868c8dd48b1c37044062fb1d4044747730cf
giangca3010 <tranducmanha01@gmail.com>
2020-09-30 14:33:23 +0700

    update date setting

--- resources/views/componentShow/footer.blade.php
+++ resources/views/componentShow/footer.blade.php
@@ -3,8 +3,8 @@
     <div class="footer-bottom">
         <div class="container">
             <div class="row">
-                {!! getConfigValueFormSettingTable('Footer_infomation')->config_value !!}
-
+{{--                {!! getConfigValueFormSettingTable('Footer_infomation')->config_value !!}--}}
+                <p class="pull-left">Copyright © 2013 G-CA Inc. </p> <p class="pull-right"> Designed by <span> <a target="_blank" href="http://www.fb.com/giangca89">GCmobile</a></span> </p>
             </div>
         </div>
     </div>
--- resources/views/componentShow/header.blade.php
+++ resources/views/componentShow/header.blade.php
@@ -6,25 +6,31 @@
                 <div class="col-sm-6">
                     <div class="contactinfo">
                         <ul class="nav nav-pills">
-                            <li><a href="#"><i
-                                        class="fa fa-phone"></i>{{ getConfigValueFormSettingTable('phone_contact')->config_value }}
-                                </a></li>
-                            <li><a href="#"><i
-                                        class="fa fa-envelope"></i> {{ getConfigValueFormSettingTable('Email')->config_value }}
-                                </a></li>
+                            <li><a href="#">
+                                    <i class="fa fa-phone"></i>
+{{--                                    {{ getConfigValueFormSettingTable('phone_contact')->config_value }}--}}
+                                    0989480463
+                                </a>
+                            </li>
+                            <li><a href="#">
+                                    <i class="fa fa-envelope"></i>
+{{--                                    {{ getConfigValueFormSettingTable('Email')->config_value }}--}}
+                                    gcgc89@gmail.com
+                                </a>
+                            </li>
                         </ul>
                     </div>
                 </div>
                 <div class="col-sm-6">
                     <div class="social-icons pull-right">
                         <ul class="nav navbar-nav">
-                            <li><a href="{{ getConfigValueFormSettingTable('Facebook_link')->config_value }}"><i
+                            <li><a href=""><i
                                         class="fa fa-facebook"></i></a></li>
-                            <li><a href="{{ getConfigValueFormSettingTable('Instagram_link')->config_value }}"><i
+                            <li><a href=""><i
                                         class="fa fa-twitter"></i></a></li>
-                            <li><a href="{{ getConfigValueFormSettingTable('Facebook_link')->config_value }}"><i
+                            <li><a href=""><i
                                         class="fa fa-linkedin"></i></a></li>
-                            <li><a href="{{ getConfigValueFormSettingTable('Instagram_link')->config_value }}"><i
+                            <li><a href=""><i
                                         class="fa fa-dribbble"></i></a></li>
                             <li><a href="#"><i class="fa fa-google-plus"></i></a></li>
                         </ul>
