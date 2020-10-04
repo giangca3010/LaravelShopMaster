33c5e758f0636e75b6bd5115774b7831e9351721
giangca3010 <tranducmanha01@gmail.com>
2020-10-04 15:53:38 +0700

    Edit images product

--- database/seeds/SliderSeeder.php
+++ database/seeds/SliderSeeder.php
@@ -16,20 +16,20 @@ class SliderSeeder extends Seeder
             [
                 'name' => 'Ấm Áp Noel Sắm Ngay Điện Thoại Mới',
                 'description' => 'Tặng kèm dịch vụ ông già Noel khi mua điện thoại trước ngày 23/12',
-                'image_path' => 'Eshopper/images/home/phone1.png',
-                'image_name' => 'iphone-11-red-400x400.jpg'
+                'image_path' => 'Eshopper/images/home/samsung-galaxy-s20.jpg',
+                'image_name' => 'samsung-galaxy-s20.jpg'
             ],
             [
                 'name' => 'Khuyến Mại 50% Tưng Bừng Trung Thu',
                 'description' => 'Trung thu tưng bừng khuyến mại mua 1 tặng 1. Mua một điện thoại tặng ngay một ốp điện thoại..',
-                'image_path' => 'Eshopper/images/home/phone1.png',
-                'image_name' => 'iphone-11-red-400x400.jpg'
+                'image_path' => 'Eshopper/images/home/samsung-galaxy-note-10-plus.jpg',
+                'image_name' => 'samsung-galaxy-note-10-plus.jpg'
             ],
             [
                 'name' => 'Sale 70% Tưng Bừng Quốc Khánh',
                 'description' => 'Quốc khánh mua 1 tặng 2. Mua một điện thoại tặng ngay hai móc chìa khóa..',
-                'image_path' => 'Eshopper/images/home/phone1.png',
-                'image_name' => 'iphone-11-red-400x400.jpg'
+                'image_path' => 'Eshopper/images/home/oppo-a52-black.jpg',
+                'image_name' => 'oppo-a52-black.jpg'
             ],
 
         ]);
